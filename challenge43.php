a76e55b1e271c550440c806a09bb99fc6d943032
Mohamamd <and>
2021-09-05 14:51:57 +0430

    add more comment for challenage 43

--- challenge43.php
+++ challenge43.php
@@ -3,10 +3,15 @@
 /** Some basic arithmetic operators are +, -, *, /, and %.
  * In this challenge you will be given three parameters, $num1, $num2, and an $operator.
  * Use the operator on number 1 and 2.
+ * There will not be any divisions by zero.
  */
+
+
 
 function operator($num1, $num2, $operator)
 {
+    if ($num2 == 0 || $num2 == 0) return "Invalid";
+
     switch ($operator) {
         case '+':
             $result = $num1 + $num2;
