eaa5e71ab0abd58ec6e3c14b192d43cfd28b0a5d
Mohamamd <and>
2021-09-05 14:31:57 +0430

    solved challenge 42

--- challenge42.php
+++ challenge42.php
@@ -1,3 +1,17 @@
 <?php
 
-// we'll do
+/** Create a function that finds the index of a given item.
+If the item is not present, return -1.
+ */
+
+function search($arr,$item){
+
+    if ($item == $arr[0]) return 0;
+
+    if ($index = array_search($item,$arr)) return $index;
+
+    return  -1;
+}
+
+echo "<pre>";
+die(var_dump(search([5,6,7],8)));
