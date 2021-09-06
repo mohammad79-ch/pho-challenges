9e3d631af5bc2aa0be67c6af4a62396580e537c1
Mohamamd <and>
2021-09-06 15:22:20 +0430

    add challenge number 52

---
+++ challenge52.php
@@ -0,0 +1,14 @@
+<?php
+
+/** Create a function that takes a string and returns a string with its letters in alphabetical order. */
+
+function AlphabetSoup($str) {
+
+    $arr = str_split($str);
+    sort($arr);
+
+    return implode('',$arr);
+}
+
+echo "<pre>";
+die(var_dump(AlphabetSoup('javascript')));
