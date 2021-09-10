705bac66565ecd5eb6959a31f1d6765916a1f90e
Mohamamd <and>
2021-09-10 13:50:39 +0430

    add the challenge number 54

--- challenge54.php
+++ challenge54.php
@@ -1,2 +1,17 @@
 <?php
 
+/** Write a function that searches an array of names (unsorted) for the
+ * name "Bob" and returns the location in the array.
+ * If Bob is not in the array, return -1.
+ */
+
+function findBob($names) {
+
+   $bobIndex = array_search('Bob',$names);
+   
+   return  is_integer($bobIndex) ? "Index ". $bobIndex : -1;
+}
+
+
+echo "<pre>";
+die(var_dump(findBob(["Mamad","Bob"])));
